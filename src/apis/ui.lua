115e442ae6129c51f4a49bf9c8dbc2c483cbad45
Ale2610 <aleproto00@gmail.com>
2015-05-24 22:07:09 +0200

    just some fixes

--- src/apis/ui.lua
+++ src/apis/ui.lua
@@ -17,7 +17,8 @@ function box(x, y, x2, y2, color) --from graphic API for FireBox
   end
   term.setCursorPos(ox, oy)
 end
-	box(1,1,51,3, colors.red)
+	local w, h = term.getSize()
+	box(1,1,w,3, colors.red)
 	term.setTextColor(colors.white)
 	center(2, "FireBox")
 	term.setBackgroundColor(colors.white)
@@ -268,7 +269,7 @@ function menu(items, title, start,allowNil,moreTitle)
 			elseif eventData[2] == keys.right then
 				changePage(1)
 			end
-		elseif eventData[1] == 'mouse_click' then
+		elseif eventData[1] == 'mouse_click' or eventData[1] == 'monitor_touch' then
 			if eventData[4] > 7 then
 				clear()
 				selected = (eventData[4]-8+((page-1)*drawSize))+1
@@ -355,7 +356,7 @@ function yesno(text, title, start)
 				clear()
 				return selected
 			end
-		elseif eventData[1] == 'mouse_click' then
+		elseif eventData[1] == 'mouse_click' or 'monitor_touch' then
 			if eventData[4] == h-1 then
 				if eventData[3] >= math.floor(w/2)+1 and eventData[3] <= w-1 then
 					clear()
