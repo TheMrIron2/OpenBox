fa502725b31a93286235eae63c016f78f4b6f269
Ale2610 <ale.proto00@gmail.com>
2015-08-30 13:44:53 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -3,21 +3,18 @@ term.setCursorPos(1,1)
 printError("Warning")
 printError("You're trying to install FireBox")
 printError("FireBox is bugged")
-printError("You sure?")
-print("yes/no")
-c = read()
-while true do
-	if c == "yes" then
-		break
-	elseif c == "no" then
-		return
-	else
-		print("Please just type yes or no")
-	end
+printError("We're removing the software from your computer (if exists)")
+sleep(5)
+if fs.exists("/.FireBox")
+	fs.delete("/.FireBox")
+	printError("Removed FireBox")
+else
+	printError("FireBox not found")
 end
 
+printError("We saved your computer from a bugged software")
 
-
+--[[
 local files = {
 	["src/FireBox.lua"] = "/.FireBox/FireBox",
 	["src/apis/fb.lua"] = "/.FireBox/apis/fb",
@@ -128,3 +125,4 @@ term.setCursorPos(2, h-1)
 print("100% - "..tostring(filesDownloaded).."/"..tostring(fileCount))
 os.pullEvent("key")
 os.reboot()
+]]--
